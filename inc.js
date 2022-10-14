7b9d9b30205d588b1ddc5085f6c809f9eb81e2c6
naveendevella <105769642+naveendevella@users.noreply.github.com>
2022-10-14 18:06:25 +0530

    Add files via upload

---
+++ inc.js
@@ -0,0 +1,11 @@
+import React,{useState} from 'react'
+const Counter=()=>{
+    const[count,setCount]=useState(0);
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+        </div>
+    );
+}
+export default Counter;
